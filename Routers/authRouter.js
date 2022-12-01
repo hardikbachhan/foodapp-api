31ced00b19677ff1266c924bd90f367174c84258
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-12-01 22:55:04 +0530

    :fire: restructured code, created router folder

---
+++ Routers/authRouter.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bcrpyt = require("bcrypt");
+const authRouter = express.Router();
+const userModel = require("../models/userModel");
+
+authRouter
+    .route("/signup")
+    .get(getSignUp)
+    .post(postSignUp);
+
+authRouter
+    .route("/login")
+    .post(loginUser)
+
+function getSignUp(req, res) {
+    res.sendFile("/public/index.html", { root: __dirname });
+}
+
+async function postSignUp(req, res) {
+    // let { email, name, password } = req.body;
+    try {
+        let data = req.body;
+        let user = await userModel.create(data);
+        console.log(data);
+        res.json({
+            msg: "user signed up",
+            user,
+        });
+    } catch (err) {
+        res.json({
+            err: err.message,
+        });
+    }
+}
+
+async function loginUser(req, res) {
+    try {
+        let { email, password} = req.body;
+        let user = await userModel.findOne({ email });
+        if (user) {
+            // check if password matches
+            // bcrpyt -> used to check password
+            const isRegisteredUser = bcrpyt.compare(password, user.password);
+            if (isRegisteredUser) {
+                res.json({
+                    msg: "user loggin in",
+                })
+            } else {
+                res.json({
+                    msg: "wrong credentials",
+                })
+            }
+        } else {
+            res.json({ msg: "user not found"});
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.json({
+            msg: error.message,
+        })
+    }
+}
+
+module.exports = authRouter;
---
+++ Routers/userRouter.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const userRouter = express.Router();
+const userModel = require("../models/userModel");
+
+userRouter
+    .route("/")
+    .get(middleware1, getUsers)
+    .post(postUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+userRouter.route("/setcookies").get(setCookies);
+
+userRouter.route("/getcookies").get(getCookies);
+
+userRouter.route("/:name").get(getUserById);
+
+function middleware1(req, res, next) {
+    console.log("midleware 1 called");
+    next();
+}
+
+// function middleware2(req,res) {
+//     console.log("midleware 2 called");
+//     res.json({ msg: "user returned" })
+// }
+
+async function getUsers(req, res) {
+    console.log(req.query);
+    let { name, age } = req.query;
+    // let filteredData=user.filter(userObj => {
+    //     return (userObj.name==name && userObj.age==age)
+    // })
+    // res.send(filteredData);
+
+    //get all users from db
+    let allUsers = await userModel.findOne({ name: "Abhishek" });
+
+    res.json({ msg: "users retrieved", allUsers });
+    // console.log("getUser called ");
+    // next();
+}
+
+function postUser(req, res) {
+    console.log(req.body.Name);
+    //then i can put this in db
+    user.push(req.body);
+    res.json({
+        message: "Data received successfully",
+        user: req.body,
+    });
+}
+
+async function updateUser(req, res) {
+    try {
+        console.log(req.body);
+        let dataToBeUpdated = req.body;
+        // for (key in dataToBeUpdated) {
+        //     user[key] = dataToBeUpdated[key];
+        // }
+        let doc = await userModel.findOneAndUpdate(
+            { email: "abc@gmail.com" },
+            dataToBeUpdated
+        );
+        res.json({
+            message: "data updated succesfully",
+        });
+    } catch (error) {
+        res.json({
+            message: error.message,
+        });
+    }
+}
+
+async function deleteUser(req, res) {
+    try {
+        // user = {};
+        let doc = await userModel.deleteOne({ email: "abcd@gmail.com" });
+        console.log(doc);
+        res.json({
+            msg: "user has been deleted",
+        });
+    } catch (error) { }
+}
+
+function getUserById(req, res) {
+    console.log(req.params.name);
+    //let {id}=req.params;
+    // let user = db.findOne(id);
+    res.json({ msg: "user id is ", obj: req.params });
+}
+
+function setCookies(req, res) {
+    // res.setHeader("Set-Cookie", "isLoggedIn=true");
+    res.cookie("isLogginIn", true, { maxAge: 30000 });
+    res.cookie("password", 123456789, { maxAge: 30000 });
+    res.send("cookies have been set");
+}
+
+function getCookies(req, res) {
+    let cookie = req.cookies;
+    console.log(cookie);
+    res.send("cookies recieved.");
+}
+
+module.exports = userRouter;
